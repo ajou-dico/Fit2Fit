e33ba8cb5c4ba5c56aff1be6dc5b734499763f90
uomf <kj632147@gmail.com>
2022-06-08 00:25:17 +0900

    Feat: Renewal main activity navigation bar

--- app/src/main/java/com/dico/fit2fit/MainActivity.java
+++ app/src/main/java/com/dico/fit2fit/MainActivity.java
@@ -17,7 +17,6 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    public ActivityMainBinding binding;
     private FirebaseAuth mAuth;
 
     @Override
@@ -26,19 +25,7 @@ public class MainActivity extends AppCompatActivity {
         FirebaseUser currentUser = mAuth.getCurrentUser();
 
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_dashboard, R.id.navigation_recording, R.id.navigation_setting)
-                .build();
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        setContentView(R.layout.activity_main);
     }
 
 }
